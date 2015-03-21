89a82a03940d19971972679eb31da6c27453ffd0
Barnabas Bulpett <bbulpett@charter.net>
2015-03-20 21:52:45 -0400

    Switch Puma to Passenger

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_capstone-0_4_0_session'
+Rails.application.config.session_store :cookie_store, key: '_capstone-1_3_6_session'
