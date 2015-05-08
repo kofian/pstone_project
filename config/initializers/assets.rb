b0e0e1a51a8d1b50306f78dc05ade1311b0d70b2
Barnabas Bulpett <bbulpett@charter.net>
2015-05-07 20:45:00 -0400

    recompiled assets to include error pages

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,9 @@ Rails.application.config.assets.precompile += %w( winter_sunrise.css )
 Rails.application.config.assets.precompile += %w( tranquil_mountain.css )
 Rails.application.config.assets.precompile += %w( tropical_shores.css )
 Rails.application.config.assets.precompile += %w( basic_styles.css )
+Rails.application.config.assets.precompile += %w(404.html 422.html 500.html)
+Rails.application.config.assets.paths << Rails.root.join('public')
+Rails.application.config.assets.register_mime_type('text/html', '.html')
 
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
