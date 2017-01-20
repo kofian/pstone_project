de862caf7024ee0ea0e8ff19dfb71f292e5f8386
kofian <al.abdimi@gmail.com>
2017-01-21 01:15:42 +0800

    passenger stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ lock '3.4.1'
 set :application, 'pstone_project'
 set :repo_url, 'git@github.com:kofian/pstone_project.git'
 
+set :passenger_restart_with_touch, true
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
